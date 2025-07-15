27adecc45f22faa71a4ee456fc73dbdae1cf5aae
admin971 <149671652+admin971@users.noreply.github.com>
2025-07-15 14:53:50 +0530

    Update backend.tf

--- use-case-5/backend.tf
+++ use-case-5/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0.0"
 
   backend "s3" {
-    bucket = "hcl-backend-mumbai"
+    bucket = "usecasefiles"
     key    = "usecase5/terraform.tfstate"
     region = "ap-south-1"
   }
