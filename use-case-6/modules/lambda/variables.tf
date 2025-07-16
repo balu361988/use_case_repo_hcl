6abdfede114235038f673d04852f72fcb8394172
admin971 <149671652+admin971@users.noreply.github.com>
2025-07-16 18:10:37 +0530

    Update variables.tf

--- use-case-6/modules/lambda/variables.tf
+++ use-case-6/modules/lambda/variables.tf
@@ -1,6 +1,18 @@
-variable "function_name" {}
-variable "handler" {}
-variable "runtime" {}
-variable "lambda_source" {}
-variable "ec2_instance_id" {}
-variable "action" {} # start or stop
+variable "function_name" {
+type = string
+}
+variable "handler" {
+type = string
+}
+variable "runtime" {
+type = string
+}
+variable "lambda_source" {
+type = string
+}
+variable "ec2_instance_id" {
+type = string
+}
+variable "action" {
+type = string
+} # start or stop
