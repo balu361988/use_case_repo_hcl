e89416c4f322983e219a71b99b8f0b0dd2a240a9
admin971 <149671652+admin971@users.noreply.github.com>
2025-07-22 23:37:38 +0530

    Delete use-case-8/backend.tf

--- use-case-8/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "use-case-backup-file"
-    key            = "use-case-8/terraform.tfstate"
-    region         = "ap-south-1"                
-    use_lockfile = true
-
-  }
-}
