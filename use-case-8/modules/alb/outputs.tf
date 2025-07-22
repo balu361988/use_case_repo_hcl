eda18944ee81ae966fae56e2b61c65fa5da9fdf0
admin971 <149671652+admin971@users.noreply.github.com>
2025-07-23 01:50:17 +0530

    Update outputs.tf

--- use-case-8/modules/alb/outputs.tf
+++ use-case-8/modules/alb/outputs.tf
@@ -1,13 +1,19 @@
 output "alb_sg_id" {
-    value = aws_security_group.alb.id
+  value = aws_security_group.alb.id
 }
+
 output "patient_target_group_arn" {
-value  = aws_lb_target_group.patient_service.arn
+  value = aws_lb_target_group.patient_service.arn
 }
+
 output "appointment_target_group_arn" {
-    value = aws_lb_target_group.appointment_service.arn
+  value = aws_lb_target_group.appointment_service.arn
 }
-output "alb_end_point_anme" {
-  description = "The DNS name of the Load Balancer"
-  value       = aws_lb.main.dns_name
+
+output "patient_listener_rule_depends_on" {
+  value = aws_lb_listener_rule.patient_service.id
+}
+
+output "appointment_listener_rule_depends_on" {
+  value = aws_lb_listener_rule.appointment_service.id
 }
