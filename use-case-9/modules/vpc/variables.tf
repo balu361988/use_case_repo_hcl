32f61f25898db336f8eefa6a2467fd0b2a037173
root <root@ip-172-31-3-234.ap-south-1.compute.internal>
2025-07-24 05:08:53 +0000

    dd

---
+++ use-case-9/modules/vpc/variables.tf
@@ -0,0 +1,25 @@
+variable "env" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR block for the VPC"
+}
+
+variable "public_subnets" {
+  type        = list(string)
+  description = "List of public subnet CIDRs"
+}
+
+variable "private_subnets" {
+  type        = list(string)
+  description = "List of private subnet CIDRs"
+}
+
+variable "azs" {
+  type        = list(string)
+  description = "List of availability zones"
+}
+
