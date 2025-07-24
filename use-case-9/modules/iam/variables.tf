35e50d47647041dbffc8175fe5415afe952c71e3
admin971 <149671652+admin971@users.noreply.github.com>
2025-07-24 11:46:57 +0530

    Update variables.tf

--- use-case-9/modules/iam/variables.tf
+++ use-case-9/modules/iam/variables.tf
@@ -6,5 +6,12 @@ variable "env" {
 variable "worker_node_policy_arns" {
   description = "List of IAM policy ARNs to attach to EKS worker node role"
   type        = list(string)
+  default     = [
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  ]
 }
+
+
 
