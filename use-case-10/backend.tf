41714971040c1f353cc6d613fab2c63f2e49a268
admin971 <149671652+admin971@users.noreply.github.com>
2025-07-27 03:24:33 +0530

    Create backend.tf

---
+++ use-case-10/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "usecasebackupfile"
+    key            = "use-case-10/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock-table"
+  }
+}
