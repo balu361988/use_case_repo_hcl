01add27a6adea5cecf3ad2fa98def92cd694ddb4
admin971 <149671652+admin971@users.noreply.github.com>
2025-07-26 22:17:13 +0530

    Create outputs.tf

---
+++ use-case-10/ modules/ecs/alb/outputs.tf
@@ -0,0 +1,14 @@
+output "alb_dns" {
+  value = aws_lb.main.dns_name
+}
+
+output "patient_target_group_arn" {
+  value = aws_lb_target_group.patient.arn
+}
+
+output "appointment_target_group_arn" {
+  value = aws_lb_target_group.appointment.arn
+}
+output "listener_arn" {
+  value = aws_lb_listener.http.arn
+}
