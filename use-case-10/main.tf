bc2cdf4d6a7ac8448279851433d05ae254ebc45c
admin971 <149671652+admin971@users.noreply.github.com>
2025-07-26 22:22:45 +0530

    Update main.tf

--- use-case-10/main.tf
+++ use-case-10/main.tf
@@ -1,1 +1,39 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source         = "./modules/vpc"
+  cidr_block     = var.vpc_cidr
+  subnet1_cidr   = var.subnet1_cidr
+  subnet2_cidr   = var.subnet2_cidr
+  az1            = var.az1
+  az2            = var.az2
+}
+
+module "ecr" {
+  source = "./modules/ecr"
+}
+
+module "iam" {
+  source = "./modules/iam"
+}
+
+module "alb" {
+  source     = "./modules/alb"
+  subnet_ids = module.vpc.subnet_ids
+  alb_sg_id  = var.alb_sg_id
+  vpc_id     = module.vpc.vpc_id
+}
+
+module "ecs" {
+  source                      = "./modules/ecs"
+  execution_role_arn         = module.iam.ecs_task_execution_role_arn
+  patient_image              = "${module.ecr.ecr_patient_url}:latest"
+  appointment_image          = "${module.ecr.ecr_appointment_url}:latest"
+  subnet_ids                 = module.vpc.subnet_ids
+  service_sg_id              = var.service_sg_id
+  patient_target_group_arn   = module.alb.patient_target_group_arn
+  appointment_target_group_arn = module.alb.appointment_target_group_arn
+}
 
