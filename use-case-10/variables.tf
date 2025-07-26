875e0724558b3014f75c48d7e64e7fb003151e67
admin971 <149671652+admin971@users.noreply.github.com>
2025-07-26 22:56:50 +0530

    Update variables.tf

--- use-case-10/variables.tf
+++ use-case-10/variables.tf
@@ -6,4 +6,16 @@ variable "az1" {}
 variable "az2" {}
 variable "alb_sg_id" {}
 variable "service_sg_id" {}
+variable "patient_repo" {
+  type        = string
+  description = "ECR repo name for patient service"
+  default     = "patient-service"
+}
+
+variable "appointment_repo" {
+  type        = string
+  description = "ECR repo name for appointment service"
+  default     = "appointment-service"
+}
+
 
