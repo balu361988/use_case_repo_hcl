9d8a5b5a178eed2eee6fc9c2f6ece0735783a01e
admin971 <149671652+admin971@users.noreply.github.com>
2025-07-26 22:13:15 +0530

    Update main.tf

--- use-case-10/ modules/ecr/main.tf
+++ use-case-10/ modules/ecr/main.tf
@@ -1,15 +1,21 @@
 resource "aws_ecr_repository" "patient" {
-  name = "patient-service"
+  name                 = var.patient_repo
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = {
+    Name = "patient-ecr"
+  }
 }
 
 resource "aws_ecr_repository" "appointment" {
-  name = "appointment-service"
-}
-
-output "ecr_patient_url" {
-  value = aws_ecr_repository.patient.repository_url
-}
-
-output "ecr_appointment_url" {
-  value = aws_ecr_repository.appointment.repository_url
+  name                 = var.appointment_repo
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = {
+    Name = "appointment-ecr"
+  }
 }
