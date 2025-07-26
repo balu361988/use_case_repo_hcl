7fc1a52959a360a8c403cfdccfdd7773c8e65d01
admin971 <149671652+admin971@users.noreply.github.com>
2025-07-27 03:26:53 +0530

    Delete use-case-10/backend.tf

--- use-case-10/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "usecasebackupfile"
-    key            = "use-case-10/terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-    dynamodb_table = "terraform-lock-table"
-  }
-}
